887fc0a0f7b2ccd11387a2620a4363f7a7573b67
rafacouceiro <rafael.balseiro@udc.es>
2026-02-26 09:22:22 +0100

    fix: noise investigation method

--- Assets/Agente/Behavior/CompoundTasks/InvestigationTask.cs
+++ Assets/Agente/Behavior/CompoundTasks/InvestigationTask.cs
@@ -10,7 +10,8 @@ namespace AgenticPrison.Behavior.CompoundTasks {
 
         public InvestigationTask() {
             Methods = new List<IMethod> {
-                new InvestigateEscapeMethod()
+                new InvestigateEscapeMethod(),
+                new InvestigateNoiseMethod()
             };
         }
     }
